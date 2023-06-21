62abb9a541dedd843cce427d8d2083df8eb04bef
Matheus Presotto Limonta <matheuslimonta@hotmail.com>
2023-06-21 16:47:22 +0000

    new ipynb

--- assmblGen.py
+++ assmblGen.py
@@ -1,10 +1,11 @@
-import numpy as np
 import pandas as pd
 import sys
 
 def main():
     print(sys.argv[1])
     print(sys.argv[2])
-    return 0
+    df = pd.read_csv("test.smbtbl", delimiter='\t')
+    print(df["MLo"])
+
 if __name__ == "__main__":
     main()
